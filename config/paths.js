28291cb30829b546d68f82ce087c4403422b2dd6
Mauricio Farinelli <mauricio@gannett.com>
2019-03-17 21:10:27 -0400

    cleanup

--- config/paths.js
+++ config/paths.js
@@ -72,7 +72,7 @@ module.exports = {
   appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
-  appIndexJs: resolveApp('src/index.js'), // CRL: Updated for demo purposes
+  appIndexJs: resolveApp('src/index.js'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
   yarnLockFile: resolveApp('yarn.lock'),
